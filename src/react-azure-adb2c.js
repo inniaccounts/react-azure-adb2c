b2fec06de07377be3d814988784457faba4ef8c3
alemkin@netchexonline.com <alemkin@netchexonline.com>
2019-05-13 12:50:48 -0400

    Updated to clean up browser storage on initialize call and updated to version 0.2.7

--- src/react-azure-adb2c.js
+++ src/react-azure-adb2c.js
@@ -4,6 +4,10 @@
 import * as Msal from 'msal'
 import React from 'react'
 
+const LOCAL_STORAGE = 'localStorage'
+const SESSION_STORAGE = 'sessionStorage'
+const AUTHORIZATION_KEY = 'Authorization'
+
 const state = {
   stopLoopingRedirect: false,
   launchApp: null,
@@ -12,53 +16,60 @@ const state = {
   cacheLocation: null
 }
 
-function authCallback(errorDesc, token, error, tokenType) {
+function authCallback (errorDesc, token, error, tokenType) {
   if (errorDesc && errorDesc.indexOf('AADB2C90118') > -1) {
     redirect()
-  }
-  else if (errorDesc) {
+  } else if (errorDesc) {
     state.stopLoopingRedirect = true
   } else {
     acquireToken()
-  }  
+  }
 }
 
-function redirect() {
-    acquireToken()
-  }
+function redirect () {
+  acquireToken()
+}
 
-function acquireToken(successCallback) {
+function acquireToken (successCallback) {
   const user = window.msal.getUser(state.scopes)
-  if (!user){
+  if (!user) {
     window.msal.loginRedirect(state.scopes)
-  }
-  else {
-      window.msal.acquireTokenSilent(state.scopes).then(accessToken => {
-        if (state.cacheLocation == 'localStorage'){
-          localStorage.setItem('Authorization', 'Bearer ' + accessToken);
-        } else {
-          sessionStorage.setItem('Authorization', 'Bearer ' + accessToken);
-        }
+  } else {
+    window.msal.acquireTokenSilent(state.scopes).then(accessToken => {
+      if (state.cacheLocation === LOCAL_STORAGE) {
+        window.localStorage.setItem(AUTHORIZATION_KEY, 'Bearer ' + accessToken)
+      } else {
+        window.sessionStorage.setItem(AUTHORIZATION_KEY, 'Bearer ' + accessToken)
+      }
 
-        state.accessToken = accessToken
-        if (state.launchApp) {
-            state.launchApp()
-        }
-        if (successCallback) {
-            successCallback()
-        }
-        }, error => {
-            if (error) {
-                window.msal.acquireTokenRedirect(state.scopes)
-            }
-        })
-  }    
+      state.accessToken = accessToken
+      if (state.launchApp) {
+        state.launchApp()
+      }
+      if (successCallback) {
+        successCallback()
+      }
+    }, error => {
+      if (error) {
+        window.msal.acquireTokenRedirect(state.scopes)
+      }
+    })
+  }
+}
+
+const cleanUpStorage = cacheLocation => {
+  if (cacheLocation === LOCAL_STORAGE) {
+    window.localStorage.removeItem(AUTHORIZATION_KEY)
+  } else if (cacheLocation === SESSION_STORAGE) {
+    window.sessionStorage.removeItem(AUTHORIZATION_KEY)
+  }
 }
 
 const authentication = {
   initialize: (config) => {
     const instance = 'https://login.microsoftonline.com/tfp/'
     const authority = `${instance}${config.tenant}/${config.signInPolicy}`
+    cleanUpStorage(config.cacheLocation)
     let scopes = config.scopes
     if (!scopes || scopes.length === 0) {
       console.log('To obtain access tokens you must specify one or more scopes. See https://docs.microsoft.com/en-us/azure/active-directory-b2c/active-directory-b2c-access-tokens')
@@ -67,12 +78,12 @@ const authentication = {
     state.scopes = scopes
     state.cacheLocation = config.cacheLocation
 
-    if (config.redirectUri){
+    if (config.redirectUri) {
       new Msal.UserAgentApplication(
         config.clientId,
         authority,
         authCallback,
-        { 
+        {
           cacheLocation: config.cacheLocation,
           redirectUri: config.redirectUri,
           postLogoutRedirectUri: config.postLogoutRedirectUri
@@ -83,31 +94,31 @@ const authentication = {
         config.clientId,
         authority,
         authCallback,
-        { 
+        {
           cacheLocation: config.cacheLocation
         }
       )
     }
   },
   run: (launchApp) => {
-    state.launchApp = launchApp; 
+    state.launchApp = launchApp
     if (!window.msal.isCallback(window.location.hash) && window.parent === window && !window.opener) {
       if (!state.stopLoopingRedirect) {
         acquireToken()
       }
     }
   },
-  required: (WrappedComponent, renderLoading) =>  {
+  required: (WrappedComponent, renderLoading) => {
     return class extends React.Component {
-      constructor(props) {
+      constructor (props) {
         super(props)
         this.state = {
           signedIn: false,
-          error: null,
+          error: null
         }
       }
 
-      componentWillMount() {
+      componentWillMount () {
         acquireToken(() => {
           this.setState({
             signedIn: true
@@ -115,13 +126,13 @@ const authentication = {
         })
       }
 
-      render() {
+      render () {
         if (this.state.signedIn) {
           return (<WrappedComponent {...this.props} />)
         }
         return typeof renderLoading === 'function' ? renderLoading() : null
       }
-    };
+    }
   },
   signOut: () => window.msal.logout(),
   getAccessToken: () => state.accessToken
